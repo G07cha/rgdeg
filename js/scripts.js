e166a10f5267e3b156acbacffb56183d74e1baeb
Konstantin Azizov <azizovkostya97@gmail.com>
2016-05-08 16:27:55 +0300

    Add scrolling to store buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+function scrollToForm() {
+	$('html, body').animate({
+    scrollTop: $("#email").offset().top
+  }, 1500, function() {
+  	$("#email").focus();
+  });
+}
+
 function subscribe() {
 	var email = document.getElementById('email').value;
 	$.get({
